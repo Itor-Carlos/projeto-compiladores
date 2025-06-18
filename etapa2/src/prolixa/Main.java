66d8a6f321265ab42659cfd9aa508e672f37b5e8
Itor Carlos <itor51carlos@gmail.com>
2025-06-18 04:28:28 -0300

    segunda etapa - lexico

---
+++ etapa2/src/prolixa/Main.java
@@ -0,0 +1,29 @@
+package prolixa;
+import prolixa.lexer.*;
+import prolixa.node.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String arquivo = "teste_prolixa/teste_prolixa.calc";
+
+			Lexer lexer =
+					new Lexer(
+							new PushbackReader(  
+									new FileReader(arquivo), 1024)); 
+			Token token;
+			while(!((token = lexer.next()) instanceof EOF)) {
+				System.out.println(token.getClass());
+				System.out.println(" ( "+token.toString()+")");
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
